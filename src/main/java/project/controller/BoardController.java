877f53ec6575d519f67c9b0bf08e3a31bbf2de99
Administrator <Administrator@304-28>
2023-01-02 19:09:39 +0900

    sumin board

---
+++ src/main/java/project/controller/BoardController.java
@@ -0,0 +1,43 @@
+package project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	//공지사항 게시판 리스트
+	@GetMapping("/Board/notice-list")
+    public String noticeList() {
+        return "Board/notice-list";
+    }
+	
+	//공지사항 글쓰기 페이지
+	@GetMapping("/Board/notice-write")
+    public String noticeWrite() {
+        return "Board/notice-write";
+    }
+	
+	//건의 게시판 리스트
+	@GetMapping("/Board/suggestion-list")
+    public String suggestionList() {
+        return "Board/suggestion-list";
+    }
+	
+	//건의사항 글쓰기 페이지
+	@GetMapping("/Board/suggestion-write")
+    public String suggestionWrite() {
+        return "Board/suggestion-write";
+    }
+	//자유 게시판 리스트
+	@GetMapping("/Board/free-list")
+    public String freeList() {
+        return "Board/free-list";
+    }
+	
+	//자유게시판 글쓰기 페이지
+	@GetMapping("/Board/free-write")
+    public String freeWrite() {
+        return "Board/free-write";
+    }
+}
