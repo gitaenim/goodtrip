ec5fc43a5798b3da3c06c0f2c3066dde38037fbd
hhana <ujb773@gmail.com>
2023-01-05 15:39:55 +0900

    근태 및 휴가 주석 달아주었습니다

--- src/main/java/project/controller/AttendanceController.java
+++ src/main/java/project/controller/AttendanceController.java
@@ -5,37 +5,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AttendanceController {
-	
+	//230104 한아 작성
+	//근태 리스트
 	@GetMapping("/attendanceList")
 	public String attendenceList() {
 		return "AttendanceMgmt/attendanceList";
 	}
-	
+	//내 근태 및 휴가
 	@GetMapping("/myAttendance")
 	public String myAttendance() {
 		return "AttendanceMgmt/myAttendance";
 	}
-	
+	//내 근무
 	@GetMapping("/myWorkingDay")
 	public String myWorkingDay() {
 		return "AttendanceMgmt/myWorkingDay";
 	}
-	
+	//내 휴가
 	@GetMapping("/myDayOff")
 	public String myDayOff() {
 		return "AttendanceMgmt/myDayOff";
 	}
-	
+	//[김트립]님 근태 및 휴가
 	@GetMapping("/personalAttendance")
 	public String personalAttendance() {
 		return "AttendanceMgmt/personalAttendance";
 	}
-	
+	//[김트립]님 근무
 	@GetMapping("/personalWorkingDay")
 	public String personalWorkingDay() {
 		return "AttendanceMgmt/personalWorkingDay";
 	}
-	
+	//[김트립]님 휴가
 	@GetMapping("/personalDayOff")
 	public String personalDayOff() {
 		return "AttendanceMgmt/personalDayOff";
