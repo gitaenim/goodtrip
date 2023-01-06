c958795bdc339a4da27fe43b05849832d0fee83b
Moon's <moon@project.com>
2023-01-06 13:10:33 +0900

    건의사항 게시판 상세페이지에 댓글 기능 추가

--- src/main/java/project/controller/SuggestionBoardController.java
+++ src/main/java/project/controller/SuggestionBoardController.java
@@ -6,8 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import project.domain.DTO.BoardSuggestionsDTO;
+import project.domain.DTO.ReplySuggestionsDTO;
+import project.service.ReplySuggestionService;
 import project.service.SuggestionBoardService;
 
 @Controller
@@ -15,6 +18,9 @@ public class SuggestionBoardController {
 
 	@Autowired
 	SuggestionBoardService suggestionservice;
+
+	@Autowired
+	ReplySuggestionService replySuggestionService;
 
 	// 건의 게시판 리스트
 	@GetMapping("/Board/suggestionList")
@@ -39,7 +45,13 @@ public class SuggestionBoardController {
 	// 건의사항 게시글에서 제목이 눌렀을 경우 해당 게시글 상세 페이지로 이동
 	@GetMapping("/suggestDetail")
 	public String suggestDetail(@RequestParam long suggestNo, Model model) {
+
+		// 게시글 정보를 가져오는 기능
 		suggestionservice.detail(suggestNo, model);
+
+		// 해당 게시글의 댓글정보를 가져오는 기능
+		replySuggestionService.findAllList(suggestNo, model);
+
 		return "Board/suggestionDetail";
 	}
 
@@ -48,6 +60,16 @@ public class SuggestionBoardController {
 	@GetMapping("/Board/suggestionDetail")
 	public String suggestionDetail() {
 		return "Board/suggestionDetail";
+	}
+
+	///// 댓글관련 //////
+
+	// 댓글 작성시 저장 후 해당 게시글의 디테일 페이지로 리다이렉트
+	@PostMapping("/ReplySuggestionWrite")
+	public String ReplySuggestionWrite(ReplySuggestionsDTO dto, RedirectAttributes redirect) {
+		replySuggestionService.save(dto);
+		redirect.addAttribute("suggestNo", dto.getSuggestNo());
+		return "redirect:/suggestDetail";
 	}
 
 }
