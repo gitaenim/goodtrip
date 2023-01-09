f28e4925c6018a1aa841715af4fb6109b2176648
togla4062 <111475795+togla4062@users.noreply.github.com>
2023-01-09 11:08:49 +0900

    Update LogController.java

--- src/main/java/project/controller/LogController.java
+++ src/main/java/project/controller/LogController.java
@@ -2,14 +2,21 @@ package project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class LogController {
+public class logController {
 
 	//로그인 페이지
-	@GetMapping("/login/login")
+	@GetMapping("/login")
     public String login() {
         return "login/login";
     }
+	
+	@GetMapping("/login_success")
+    public String loginSuccexx() {
+        return "index";
+    }
+	
 		
 }
