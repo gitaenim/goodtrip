d19fa9d2e4abd9cd3df3690122910b775c1db4c8
togla4062 <hon78974@gmail.com>
2023-01-10 11:54:40 +0900

    Entity 수정 및 건의 게시판 완료

--- src/main/java/project/domain/entity/BoardSuggestionsEntity.java
+++ src/main/java/project/domain/entity/BoardSuggestionsEntity.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "board_notices")
+@Table(name = "board_suggestions")
 @Entity
 @Getter
 //230104 안나 생성
