a1f19dff1ab094a774b437ec3c1da0d2d52f256b
Administrator <Administrator@304-13>
2023-01-10 14:19:31 +0900

    0110 휴가등록 수정 거의완료

--- src/main/java/project/domain/DTO/DayOffListDTO.java
+++ src/main/java/project/domain/DTO/DayOffListDTO.java
@@ -3,6 +3,8 @@ package project.domain.DTO;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 import project.domain.entity.DayOffEntity;
 import project.domain.entity.DaysOffNumbersEntity;
@@ -13,19 +15,25 @@ public class DayOffListDTO {
 	//휴가 리스트 DTO
 	
 	private long dayOffNo; //휴가번호
+	
 	private String type; //휴가종류
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDateTime startDate; //휴가시작일
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDateTime EndDate; //휴가종료일
+	
 	private DaysOffNumbersEntity useDays; //휴가일수
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate draftDate; //기안일
 	
-	public DayOffListDTO(DayOffEntity e) {
-		this.dayOffNo = e.getDayOffNo();
-		this.type = e.getType();
-		this.startDate = e.getStartDate();
-		EndDate = e.getEndDate();
-		this.useDays = e.getUseDays();
-		this.draftDate = e.getDraftDate();
-	}
+	/*
+	 * public DayOffListDTO(DayOffEntity e) { this.dayOffNo = e.getDayOffNo();
+	 * this.type = e.getType(); this.startDate = e.getStartDate(); this.EndDate =
+	 * e.getEndDate(); this.useDays = e.getUseDays(); this.draftDate =
+	 * e.getDraftDate(); }
+	 */
 	
 }
