72397eef83c1b52c28d78371581b4bf809b975cb
Administrator <Administrator@304-28>
2023-01-11 17:29:30 +0900

    sumin

---
+++ src/main/java/project/controller/personnelEvaController.java
@@ -0,0 +1,20 @@
+package project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class personnelEvaController {
+	
+	// 인사관리 평가 메인리스트페이지
+		@GetMapping("/personnelEvaList")
+		public String personnelEvaMain() {
+			return "personnel/persommelEvaList";
+	}
+	
+	// 인사관리 평가 페이지
+	@GetMapping("/personnelEva")
+	public String personnelEva() {
+		return "personnel/personnelEva";
+	}
+}
