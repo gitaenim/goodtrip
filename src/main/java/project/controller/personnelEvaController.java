84fa562da9906045a126c90ecc4f4fa61e0db476
Administrator <Administrator@304-28>
2023-01-12 17:03:24 +0900

    sumini

--- src/main/java/project/controller/personnelEvaController.java
+++ src/main/java/project/controller/personnelEvaController.java
@@ -1,20 +1,30 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import project.service.OrganizationChartService;
 
 @Controller
 public class personnelEvaController {
 	
-	// 인사관리 평가 메인리스트페이지
-		@GetMapping("/personnelEvaList")
-		public String personnelEvaMain() {
-			return "personnel/persommelEvaList";
-	}
+	@Autowired
+	OrganizationChartService organizationChartService; 
 	
+	// 인사관리 평가 메인리스트페이지
+	@GetMapping("/personnelEvaList")
+	public String personnelEvaMain(Model model) {
+		organizationChartService.findAllList(model);
+		return "personnel/persommelEvaList";
+	}
 	// 인사관리 평가 페이지
 	@GetMapping("/personnelEva")
 	public String personnelEva() {
 		return "personnel/personnelEva";
 	}
+
+
 }
