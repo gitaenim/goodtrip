79b988829547b2d7bd006b2d27755676321c69ee
hhana <ujb773@gmail.com>
2023-01-25 12:06:50 +0900

    css수정

--- src/main/java/project/enums/MyRole.java
+++ src/main/java/project/enums/MyRole.java
@@ -10,7 +10,8 @@ public enum MyRole {
 
 	EMPLOYEE("ROLE_EMPLOYEE"),
 	PERSONALMANAGER("ROLE_PERSONAL"),
-	NONE("ROLE_RESIGNED"); //230120 한아 추가
+	NONE("ROLE_RESIGNED"), //230120 한아 추가
+	CEO("ROLE_CEO");
 	
 	private final String role;
 
