850411f1ef7a736b2d153b01beb68b39f41e7e3c
Administrator <Administrator@304-28>
2023-01-25 14:46:27 +0900

    인사 평가 권한부여

--- src/main/java/project/enums/MyRole.java
+++ src/main/java/project/enums/MyRole.java
@@ -7,10 +7,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MyRole {
 	
-
+	
 	EMPLOYEE("ROLE_EMPLOYEE"),
 	PERSONALMANAGER("ROLE_PERSONAL"),
-	NONE("ROLE_RESIGNED"); //230120 한아 추가
+	NONE("ROLE_RESIGNED"),
+	CEO("ROLE_CEO"); //230120 한아 추가
 	
 	private final String role;
 
