8710a2031866e9ddcf905611a9a16169c7825702
hhana <ujb773@gmail.com>
2023-01-25 17:57:40 +0900

    챗봇 업그레이드, 사이드메뉴 스크롤

--- src/test/java/project/PinkBalloonApplicationTests.java
+++ src/test/java/project/PinkBalloonApplicationTests.java
@@ -43,5 +43,45 @@ class PinkBalloonApplicationTests {
 				.content("문의한 사원의 전화번호입니다.")
 				.build());
 	}
+	//@Test
+	void 안녕의도() {
+		intention.save(ChatBotIntention.builder()
+				.name("안녕")
+				.answer(answer.save(Answer.builder()
+						.keyword("안녕")
+						.content("안녕하세요</br>HELP BOT입니다.")
+						.build()))
+				.build());
+		;
+	}
+	//@Test
+	void 기타의도() {
+		intention.save(ChatBotIntention.builder()
+				.name("기타")
+				.answer(answer.save(Answer.builder()
+						.keyword("기타")
+						.content("질문이 명확하지 않습니다</br>정확하게 입력 해 주세요!")
+						.build()))
+				.build());
+		;
+	}
+	@Test
+	void 휴가의도() {
+		/*
+		answer.save(Answer.builder()
+				.keyword("휴가신청")
+				.content("업무결재-휴가및병가 메뉴에서 신청할 수 있어요!")
+				.build());
+				*/
+		intention.save(ChatBotIntention.builder()
+				.name("신청")
+				.upper(intention.save(ChatBotIntention.builder()
+						.name("휴가")
+						.build()))
+				.answer(answer.findByKeyword("휴가신청").get())
+				.build());
+		
+		
+	}
 	//--------------------------------------//
 }
